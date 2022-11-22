9702ece53ed0fddc5079de33d9dedc5072d4f4a6
estherakinwumi <estherakinwumi440@gmail.com>
2022-11-22 17:04:31 +0100

    all files added

---
+++ scp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This script will be use to copy files over remote servers
+read -p "Please enter a filename: " filename
+cat /etc/passwd > $filename
+echo "we are testing scp command" >> $filename
+sudo chmod +x $filename
+scp -i key $filename ubuntu@172.31.21.93:/tmp
+echo "Please make sure you watch the /tmp of the remote server "
