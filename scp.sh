d3abd9949975fc3b87049d6990981427fa0aaa92
akins <akins440@mail.com>
2022-11-22 19:36:41 +0000

    testing

--- scp.sh
+++ scp.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-#This script will be use to copy files over remote servers
-#read -p "Please enter a filename: " filename
-#echo "we are testing scp command" >> $filename
-#sudo chmod +x $filename
-#scp -i key $filename ubuntu@172.31.21.93:/tmp
 echo "Please make sure you watch the /tmp of the remote server "
 echo "testing git security"
